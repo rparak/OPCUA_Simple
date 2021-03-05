2bce34e3b0a62ee146f690af36b01dd2cfb33705
Roman Parak <59449320+rparak@users.noreply.github.com>
2021-03-05 18:21:47 +0000

    Update Program.cs

--- C_Sharp_Client/Opcua_client/Opcua_client/Program.cs
+++ C_Sharp_Client/Opcua_client/Opcua_client/Program.cs
@@ -20,7 +20,7 @@ SOFTWARE.
 Author   : Roman Parak
 Email    : Roman.Parak @outlook.com
 Github   : https://github.com/rparak
-File Name: ar_object_control.cs
+File Name: Program.cs
 ****************************************************************************/
 
 // ------------------------------------------------------------------------------------------------------------------------//
